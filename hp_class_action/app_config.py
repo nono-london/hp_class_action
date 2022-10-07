b544e5442ac84a73afc62194d91bfd737cc52c77
thinkcentre <mysqlprojet@gmail.com>
2022-10-07 23:45:01 +0100

    THINKCENTRE: scraps HP forum

--- hp_class_action/app_config.py
+++ hp_class_action/app_config.py
@@ -1,43 +1,10 @@
 import sys
-from os import listdir as _listdir
+from os import (makedirs as _makedirs)
 from os.path import (dirname as _dirname, abspath as _abspath,
                      join as _join, isfile as _isfile, exists as _exists)
 
-from os import (makedirs as _makedirs)
-
-from win32com.client import Dispatch
 
 from selenium_helpers.app_config_secret import SELENIUM_URL as _SELENIUM_URL
-
-
-def get_chrome_installed_version() -> str:
-    def get_version_via_com(filename):
-        parser = Dispatch("Scripting.FileSystemObject")
-        try:
-            version = parser.GetFileVersion(filename)
-        except Exception as ex:
-            # print(f'Error while getting version of installed Chrome: {ex}')
-            return None
-        return version
-
-    paths = [r"C:\Program Files\Google\Chrome\Application\chrome.exe",
-             r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"]
-    version = list(filter(None, [get_version_via_com(p) for p in paths]))[0]
-    return version
-
-
-def get_correct_chrome_version_full_path():
-    chromes_folder_path: str = get_backup_selenium_folder()
-    all_files: list = _listdir(chromes_folder_path)
-    chrome_installed_version = get_chrome_installed_version().split('.')[0]
-    print(f'This computer runs a version of Chrome: {chrome_installed_version}.xx.x.x')
-    chrome_exe: str = f'chromedriver_{chrome_installed_version}.exe'
-    if chrome_exe not in all_files:
-        exit(
-            f"You need to download correct version of Selenium for Chrome installed version: {chrome_installed_version}")
-
-    chrome_exe_full_path = _join(chromes_folder_path, chrome_exe)
-    return chrome_exe_full_path
 
 
 def get_project_root_path():
@@ -53,7 +20,7 @@ def get_backup_selenium_folder():
 
 def get_project_download_path():
     # https://stackoverflow.com/questions/25389095/python-get-path-of-root-project-structure/40227116
-    download_folder_path = _join(get_project_root_path(), "selenium_helpers", "downloads")
+    download_folder_path = _join(get_project_root_path(), "hp_class_action", "downloads")
     if not _exists(download_folder_path):
         _makedirs(download_folder_path)
     return download_folder_path
@@ -74,11 +41,6 @@ def get_selenium_folder_path():
 def pack_python_libs_in_path():
     python_app_folder_path: str = _dirname(get_project_root_path())
 
-    proxy_helpers_folder_path: str = _join(python_app_folder_path, 'proxy_helpers')
-    # insert path in 2nd position, first position is reserved
-    if proxy_helpers_folder_path not in sys.path:
-        sys.path.insert(1, proxy_helpers_folder_path)
-
     mysql_helpers_folder_path: str = _join(python_app_folder_path, 'mysql_helpers')
     # insert path in 2nd position, first position is reserved
     if mysql_helpers_folder_path not in sys.path:
@@ -89,9 +51,18 @@ def pack_python_libs_in_path():
     if postgres_folder_path not in sys.path:
         sys.path.insert(1, postgres_folder_path)
 
+    proxy_helpers_folder_path: str = _join(python_app_folder_path, 'proxy_helpers')
+    # insert path in 2nd position, first position is reserved
+    if proxy_helpers_folder_path not in sys.path:
+        sys.path.insert(1, proxy_helpers_folder_path)
+
+    selenium_folder_path: str = _join(python_app_folder_path, 'selenium_helpers')
+    # insert path in 2nd position, first position is reserved
+    if selenium_folder_path not in sys.path:
+        sys.path.insert(1, selenium_folder_path)
+
 
 if __name__ == '__main__':
-    print(get_correct_chrome_version_full_path())
     print(get_project_root_path())
     print(get_project_download_path())
     print(get_selenium_folder_path())
--- hp_class_action/hinge_issue/hp_hinge_broken.py
+++ hp_class_action/hinge_issue/hp_hinge_broken.py
@@ -1,5 +1,137 @@
+from datetime import datetime, timezone
+from typing import Union
+from urllib.parse import urljoin
+
+import pandas as pd
 import requests
-base_url= "https://h30434.www3.hp.com/t5/forums/searchpage/tab/message?filter=location&q=broken%20hinge&advanced=true&location=category:Notebook&collapse_discussion=true&search_type=thread&search_page_size=50"
-page_source = requests.get(url=base_url)
-print(page_source.text)
-print(page_source.json())
+from lxml.html import fromstring, Element
+
+
+def get_web_page(url_to_open: str) -> str:
+    headers: dict = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
+    page_content: str = Union[None, str]
+    try:
+        page_content = requests.get(url=url_to_open, headers=headers, timeout=10).text
+    except ConnectionError as ex:
+        print(f'Error while connecting:\n{ex}')
+        exit(0)
+    return page_content
+
+
+def get_page_rows(page_source: str) -> [Element]:
+    xpath_value = "// div[@class='search-result-count']"
+    # lxml examples
+    lxml_str = fromstring(page_source)
+    results_number: int = int(lxml_str.xpath(xpath_value)[0].text.strip().split(" ")[0].replace(",", "").strip())
+    print(results_number)
+
+    xpath_value = "// div[@data-lia-message-uid]"
+    page_rows: [Element] = lxml_str.xpath(xpath_value)
+    print(len(page_rows))
+    return page_rows
+
+
+def get_post_url(page_row: Element, message_id: int) -> str:
+    """Return the url of the post"""
+    xpath_value = f"// div[@data-lia-message-uid={message_id}] //*[@class='page-link lia-link-navigation lia-custom-event']"
+    href_element: Element = page_row.xpath(xpath_value)[0]
+    href = href_element.get('href')
+    href = urljoin(base=base_url, url=href)
+    return href
+
+
+def get_message_id(page_row: Element) -> int:
+    """Return the message id"""
+    msg_id: int = page_row.get('data-lia-message-uid')
+    return msg_id
+
+
+def get_username(page_row: Element, message_id: int) -> str:
+    """Return the username attached to the post"""
+    xpath_value = f"// div[@data-lia-message-uid={message_id}] // span[contains(@class,'UserName')]"
+    username_element: Element = page_row.xpath(xpath_value)[0]
+    username = username_element.text_content().strip()
+    return username
+
+
+def get_posted_datetime(page_row: Element, message_id: int) -> datetime:
+    """Return the datetime at which the post was posted"""
+    xpath_value = f"// div[@data-lia-message-uid={message_id}] //  span[@class='DateTime']"
+    posted_on_element: Element = page_row.xpath(xpath_value)[0]
+    posted_on_str: str = posted_on_element.text_content().strip().replace("\n", " ").replace("\t", " ").strip()
+    posted_on_str = posted_on_str.split(" ")[0].strip() + " " + posted_on_str.split(" ")[-2].strip() + " " + \
+                    posted_on_str.split(" ")[-1].strip()
+    posted_on_str = posted_on_str.replace('\u200e', '')
+    posted_on: datetime = datetime.strptime(posted_on_str, "%m-%d-%Y %I:%M %p")
+    LOCAL_TIMEZONE = datetime.now(timezone.utc).astimezone().tzinfo
+    posted_on = posted_on.replace(tzinfo=LOCAL_TIMEZONE)
+    return posted_on
+
+
+def get_tags(page_row: Element, message_id: int) -> [str]:
+    """Return the Tags attached to the post"""
+    # xpath_value = "// div[@class='TagList lia-component-tags lia-component-message-view-widget-tags-list'] / @id[contains(.,'tagsList')]"
+    xpath_value = f'// div[@data-lia-message-uid={message_id}] // div[contains(@id,"tagsList")] / ul[@aria-label="Tags" and @role="list"]/li '
+
+    tag_elements: [Element] = page_row.xpath(xpath_value)
+    tags: set = set()
+    for tag_element in tag_elements:
+        tag_str = tag_element.text_content().strip().replace("Tags:", "")
+        if tag_str:
+            tags.add(tag_str)
+    return list(sorted(tags))
+
+
+def get_post_summary(page_row: Element, message_id: int) -> str:
+    """Return a partial text/summary of the post"""
+    xpath_value = f'// div[@data-lia-message-uid={message_id}] // div[@class="lia-truncated-body-container"] '
+
+    summary_element: Element = page_row.xpath(xpath_value)[0]
+    summary_str = summary_element.text_content().strip().replace("\t", " ").replace("\n", " ")
+    return summary_str
+
+
+def webscrap_data(page_rows: [Element]) -> pd.DataFrame:
+    results: list = []
+    for page_row in page_rows:
+        my_post_id = get_message_id(page_row=page_row)
+
+        my_post_url = get_post_url(page_row=page_row, message_id=my_post_id)
+        my_username = get_username(page_row=page_row, message_id=my_post_id)
+        my_post_datetime = get_posted_datetime(page_row=page_row, message_id=my_post_id)
+        my_tags = get_tags(page_row=page_row, message_id=my_post_id)
+        my_summary = get_post_summary(page_row=page_row, message_id=my_post_id)
+
+        results.append({'post_id': my_post_id,
+                        'post_datetime': my_post_datetime,
+                        'username': my_username,
+                        'post_url': my_post_url,
+                        'post_tags': my_tags,
+                        'post_summary': my_summary
+                        })
+
+    result_df: pd.DataFrame = pd.DataFrame(results)
+    return result_df
+
+
+result_df = pd.DataFrame()
+results_per_page: int = 50
+offset_pages: int = int(2000 / 50) + 2
+
+for i in range(0, offset_pages):
+    base_url = f"""https://h30434.www3.hp.com/t5/forums/searchpage/tab/message?filter=location&q=broken%20hinge&advanced=true&
+    location=category:Notebook&sort_by=-topicPostDate&
+    page={i}&
+    collapse_discussion=true&search_type=thread&search_page_size={results_per_page}"""
+
+    page_source = get_web_page(url_to_open=base_url)
+    page_rows = get_page_rows(page_source=page_source)
+    temp_df: pd.DataFrame = webscrap_data(page_rows=page_rows)
+    result_df = pd.concat([result_df, temp_df], ignore_index=True, )
+
+print(result_df)
+result_df.to_csv(path_or_buf='hp_hinges_issues.csv',
+                 sep=',',
+                 index=False
+                 )
