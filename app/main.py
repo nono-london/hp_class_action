9da04418a216149c7726c35350aea91c8fbd3472
thinkcentre <mysqlprojet@gmail.com>
2022-10-09 04:28:12 +0100

    THINKCENTRE: start flask app

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
     return "<h1>Welcome HP Trial Web Information</h1>"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001,
+            debug=True)
