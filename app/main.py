e5a4a928f379b17b05edcc8b401f3efb59e06562
thinkcentre <mysqlprojet@gmail.com>
2022-10-09 04:42:57 +0100

    THINKCENTRE: flask

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_view():
-    return "<h1>Welcome HP Forum Information Checker</h1>"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- hp_class_action/hinge_issue/hp_hinge_broken.py
+++ hp_class_action/hinge_issue/hp_hinge_broken.py
@@ -1,13 +1,15 @@
 from datetime import datetime, timezone
+from pathlib import Path
 from random import randint
 from time import sleep
 from typing import Union
 from urllib.parse import urljoin
-from pathlib import Path
+
 import pandas as pd
 import requests
 from lxml.html import fromstring, Element
 from requests.exceptions import ConnectionError
+
 from hp_class_action.app_config import get_project_download_path
 from hp_class_action.hinge_issue.hp_hinge_stats import upload_data
 
