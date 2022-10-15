2bc826a5616c24589e9c3a5f39c896cc5caeecaf
hplaptop <mysqlproject@gmail.com>
2022-10-15 20:28:35 +0200

    HPLAPTOP: adding tests suite

---
+++ tests/test_mdb.py
@@ -0,0 +1,24 @@
+from hp_class_action.hp_database.hp_forum_issue import (fetch_query,
+                                                        get_connection)
+
+
+def test_connection():
+    conn = get_connection()
+    print(f'is mdb connected: {conn.is_connected()}')
+    conn.disconnect()
+    conn.close()
+    print(f'is mdb connected: {conn.is_connected()}')
+
+
+def fetch_data():
+    sql_query = """
+    SELECT *
+    FROM hp_forum_issues
+    LIMIT 5
+    """
+    print(fetch_query(sql_query=sql_query))
+
+
+if __name__ == '__main__':
+    test_connection()
+    fetch_data()
