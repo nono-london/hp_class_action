a879b8845bf7d3a230645d0ace7d28f5d8e9779d
thinkcentre <mysqlprojet@gmail.com>
2022-10-16 03:12:05 +0100

    THINKCENTRE: adding connection error from mysql

--- hp_class_action/hinge_issue/scrap_data/web_requests.py
+++ hp_class_action/hinge_issue/scrap_data/web_requests.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+from requests.exceptions import ConnectionError
 import requests
 
 BASE_URL: str = "https://h30434.www3.hp.com/t5/ratings/ratingdetailpage/message-uid/8499984/rating-system/forum_topic_metoo/page/1#userlist"
