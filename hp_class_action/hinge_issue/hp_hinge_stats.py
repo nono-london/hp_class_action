aa1e63f94bcb6cf0b2f2239dfa16fd7e3ac21f61
hplaptop <mysqlproject@gmail.com>
2022-10-18 01:27:10 +0200

    HPLAPTOP: adapting for new tables

--- hp_class_action/hinge_issue/hp_hinge_stats.py
+++ hp_class_action/hinge_issue/hp_hinge_stats.py
@@ -10,18 +10,22 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-def chart_monthly_claims():
+def chart_monthly_claims(from_year: int = 2018):
     """Charts monthly claims without last month"""
     sql_query: str = """
                 SELECT STR_TO_DATE( CONCAT_WS('-',YEAR(post_datetime), 
                             MONTH(post_datetime),1),'%Y-%m-%d') AS "Claimed Month"
                 , COUNT(*) "Monthly Claims"
                 FROM forum_posts
+                WHERE post_datetime>= STR_TO_DATE( 
+                        CONCAT_WS('-', %s, 1, 1),'%Y-%m-%d')
                 GROUP BY YEAR(post_datetime), MONTH(post_datetime)
                 ORDER BY post_datetime
         """
     result_df = pd.read_sql(con=get_connection(),
-                            sql=sql_query)
+                            sql=sql_query,
+                            params=(from_year,))
+    result_df['Claimed Month'] = pd.to_datetime(result_df['Claimed Month'])
 
     result_df.set_index("Claimed Month", inplace=True)
     # https://pandas.pydata.org/pandas-docs/version/0.13/visualization.html
@@ -31,18 +35,22 @@ def chart_monthly_claims():
     plt.show()
 
 
-def chart_yearly_claims():
+def chart_yearly_claims(from_year: int = 2018):
     """Charts yearly claims"""
     sql_query: str = """
                 SELECT STR_TO_DATE( CONCAT_WS('-',YEAR(post_datetime), 1,1),'%Y-%m-%d') 
                         AS "Claimed Year"
                 , COUNT(*) "Yearly Claims"
                 FROM forum_posts
+                WHERE post_datetime>= STR_TO_DATE( 
+                        CONCAT_WS('-', %s, 1, 1),'%Y-%m-%d')
                 GROUP BY YEAR(post_datetime)
                 ORDER BY post_datetime
         """
     result_df: pd.DataFrame = pd.read_sql(con=get_connection(),
-                                          sql=sql_query)
+                                          sql=sql_query,
+                                          params=(from_year,))
+    result_df['Claimed Year'] = pd.to_datetime(result_df['Claimed Year'])
 
     result_df.set_index("Claimed Year", inplace=True)
     # https://pandas.pydata.org/pandas-docs/version/0.13/visualization.html
@@ -52,16 +60,19 @@ def chart_yearly_claims():
     plt.show()
 
 
-def all_claims():
+def all_claims(from_year: int = 2018):
     """Returns a df with claims from post id and from people who had same issue"""
 
     sql_query: str = """
             SELECT b.username, a.hp_post_id, a.post_datetime, a.me_too
             FROM forum_posts a INNER JOIN hp_users b 
                 ON a.user_id=b.user_id
+            WHERE a.post_datetime>=STR_TO_DATE( 
+                        CONCAT_WS('-', %s, 1, 1),'%Y-%m-%d')
             ORDER BY a.post_datetime DESC
         """
-    results: list = fetch_query(sql_query=sql_query)
+    results: list = fetch_query(sql_query=sql_query,
+                                variables=(from_year,))
     for row_dict in results:
         if row_dict['me_too'] is not None:
             row_dict['me_too'] = json.loads(row_dict['me_too'])
@@ -100,7 +111,9 @@ def all_claims():
 if __name__ == '__main__':
     all_claims()
     exit(0)
+
     chart_monthly_claims()
     exit(0)
+
     chart_yearly_claims()
     exit(0)
