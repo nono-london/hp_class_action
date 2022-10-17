c3ec4af73b5e3b0029647b976e528f0e8ceb929f
hplaptop <mysqlproject@gmail.com>
2022-10-18 00:59:15 +0200

    HPLAPTOP: deleted old webscraper modules

--- hp_class_action/hinge_issue/hp_hinge_stats.py
+++ hp_class_action/hinge_issue/hp_hinge_stats.py
@@ -10,37 +10,6 @@ from hp_class_action.hp_database.hp_forum_issue import (execute_query, get_conne
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-LOCAL_FILE_NAME: str = str(Path().joinpath(get_project_download_path(), 'hp_hinges_issues.csv'))
-
-
-def read_local_data():
-    result_df = pd.read_csv(LOCAL_FILE_NAME,
-                            sep=',',
-                            parse_dates=['post_datetime'])
-    print(result_df.drop_duplicates(subset=['hp_post_id', 'post_datetime'],
-                                    keep='first'))
-    return result_df
-
-
-def upload_data(data_df: pd.DataFrame):
-    sql_query = """
-        INSERT INTO hp_forum_issues (
-            hp_post_id, post_datetime, username,
-            post_url,post_tags,post_summary
-            )
-            VALUES(
-            %s,%s,%s,
-            %s,%s,%s
-            ) 
-    """
-
-    for index, row in data_df.iterrows():
-        execute_query(sql_query=sql_query,
-                      variables=(row['hp_post_id'], row['post_datetime'], row['username'],
-                                 row['post_url'], json.dumps(row['post_tags']), row['post_summary']
-                                 )
-                      )
-
 
 def chart_monthly_claims():
     """Charts monthly claims without last month"""
@@ -136,6 +105,4 @@ if __name__ == '__main__':
     exit(0)
     chart_yearly_claims()
     exit(0)
-    my_result_df = read_local_data()
-    upload_data(my_result_df)
     chart_monthly_claims()
