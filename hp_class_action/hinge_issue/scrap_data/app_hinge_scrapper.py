62166f5272b35463263ad0e07f01f5beafe7174e
hp-elite-linux <mysqlproject@gmail.com>
2022-10-18 14:02:32 +0200

    HP-ELITE-LINUX: added app_hinge_scrapper.py to launch the full scrapping process for @hinge broken@ key words

---
+++ hp_class_action/hinge_issue/scrap_data/app_hinge_scrapper.py
@@ -0,0 +1,13 @@
+"""Launch the search scrapper and the me_too scrapper"""
+
+from hp_class_action.hinge_issue.scrap_data.scrap_search_query import webscrap_query_search
+from hp_class_action.hinge_issue.scrap_data.scrap_metoo import update_summary_metoo
+
+
+def launch_scrapper():
+    webscrap_query_search()
+    update_summary_metoo(force_update=False)
+
+
+if __name__ == '__main__':
+    launch_scrapper()
