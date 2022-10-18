ff18a4e91228d5c33b283e33b15b3110cecdfcbd
thinkcentre <mysqlprojet@gmail.com>
2022-10-18 13:32:59 +0100

    THINKCENTRE: adding streamlit support

---
+++ app_streamlit/main.py
@@ -0,0 +1,4 @@
+from hp_class_action.hinge_issue.data_analyses.histo_claims import (chart_claim_hidden_claims,
+                                                                    chart_claim_hidden_claims_as_percent)
+
+
