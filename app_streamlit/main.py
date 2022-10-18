1f6c48d207a3a8c8fd8ae606b14628d6384f292c
hplaptop <mysqlproject@gmail.com>
2022-10-18 18:43:49 +0200

    HPLAPTOP: adding streamlit

--- app_streamlit/main.py
+++ app_streamlit/main.py
@@ -1,20 +1,22 @@
-import streamlit as st
 import sys
 from pathlib import Path
+
+import streamlit as st
 
 # add project to PYTHONPATH
 sys.path.insert(1, str(Path(__file__).resolve().parent.parent))
 
-
 from hp_class_action.hinge_issue.data_analyses.histo_claims import (chart_claim_hidden_claims,
                                                                     chart_claim_hidden_claims_as_percent)
+
+FROM_YEAR: int = 2017
 
 st.header("HP Class Action")
 
 st.markdown("Number of claims split between private-messaged and forum-ed")
-fig_all_claims, _ = chart_claim_hidden_claims(show_chart=False)
-st.pyplot(fig_all_claims,)
+fig_all_claims, _ = chart_claim_hidden_claims(show_chart=False, from_year=FROM_YEAR)
+st.pyplot(fig_all_claims, )
 
 st.markdown("Number of claims in percentage split between private-messaged and forum-ed")
-fig_all_claims_percent, _ = chart_claim_hidden_claims_as_percent(show_chart=False)
+fig_all_claims_percent, _ = chart_claim_hidden_claims_as_percent(show_chart=False, from_year=FROM_YEAR)
 st.pyplot(fig_all_claims_percent)
