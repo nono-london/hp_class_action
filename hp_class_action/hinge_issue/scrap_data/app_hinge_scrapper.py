a1167ac75b6c0ea425cfec2217ae8165b97028d5
hplaptop <mysqlproject@gmail.com>
2022-10-19 00:12:17 +0200

    HPLAPTOP: added backup on app_hinge_scrapper.py

--- hp_class_action/hinge_issue/scrap_data/app_hinge_scrapper.py
+++ hp_class_action/hinge_issue/scrap_data/app_hinge_scrapper.py
@@ -2,9 +2,11 @@
 
 from hp_class_action.hinge_issue.scrap_data.scrap_search_query import webscrap_query_search
 from hp_class_action.hinge_issue.scrap_data.scrap_metoo import update_summary_metoo
+from hp_class_action.hp_database.backup_mdb import backup_mdb_to_csv
 
 
 def launch_scrapper():
+    backup_mdb_to_csv()
     webscrap_query_search(max_pages=10)
     update_summary_metoo(force_update=False)
 
