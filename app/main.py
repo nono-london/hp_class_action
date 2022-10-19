94be6a0086bb06d5853265d9a94db270bf87a259
hplaptop <mysqlproject@gmail.com>
2022-10-20 01:20:10 +0200

    adding hp_issue page

--- app/main.py
+++ app/main.py
@@ -13,15 +13,15 @@ def home_view():
                     defective HP laptops
         """
     carousel_id = "carouselHP"
-    carousel_slides: list = [{'slide_name': 'broken_hinge', 'slide_position': "0", "slide_wait_interval": 3000,
+    carousel_slides: list = [{'slide_name': 'broken_hinge', 'slide_position': 0, "slide_wait_interval": 3000,
                               'slide_title': 'Broken Hinges', 'slide_h5': 'HP Forum Support',
                               'slide_p': 'Data gathered from HP users testimonies'
                               },
-                             {'slide_name': 'broken_hinge_screen', 'slide_position': "1", "slide_wait_interval": 2000,
+                             {'slide_name': 'broken_hinge_screen', 'slide_position': 1, "slide_wait_interval": 2000,
                               'slide_title': 'Broken Hinges And Screens', 'slide_h5': 'HP Forum Support',
                               'slide_p': 'Data gathered from HP users testimonies'
                               },
-                             {'slide_name': 'battery_issue', 'slide_position': "2", "slide_wait_interval": 2000,
+                             {'slide_name': 'battery_issue', 'slide_position': 2, "slide_wait_interval": 2000,
                               'slide_title': 'Batteries', 'slide_h5': 'HP Forum Support',
                               'slide_p': 'Data gathered from HP users testimonies'
                               },
@@ -32,6 +32,11 @@ def home_view():
                                                     'carousel_id': carousel_id,
                                                     'carousel_slides': carousel_slides
                                                     })
+
+
+@app.route("/hp_issue_<issue_type>")
+def hp_issue(issue_type):
+    return render_template()
 
 
 if __name__ == '__main__':
