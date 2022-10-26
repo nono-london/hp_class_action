2eeced7ae2fe3fb9be82d1e81116718e245724d5
hplaptop <mysqlproject@gmail.com>
2022-10-26 20:46:42 +0200

    cleaning

--- app/main.py
+++ app/main.py
@@ -36,12 +36,13 @@ def home_view():
 
 @app.route("/hp_issue_<issue_type>")
 def hp_issue(issue_type):
-    page_title: str = "HP Issue"
+    page_title: str = str(issue_type.replace('_', ' ')).title()
     print("in hp issue router")
+    h2_text = "HP issue: " + page_title
+    
+    
     return render_template('hp_issue.html', page_vars={'title': page_title,
-                                                        'h2_text': "",
-                                                        'carousel_id': "",
-                                                        'carousel_slides': ""
+                                                        'h2_text': h2_text,
                                                         })
 
 
