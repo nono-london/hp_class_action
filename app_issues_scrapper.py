7c51da2f2c43af86dd7496607815eadef52ea9dc
thinkcentre <mysqlprojet@gmail.com>
2022-10-27 22:26:23 +0100

    THINKCENTRE: cleaning

--- app_issues_scrapper.py
+++ app_issues_scrapper.py
@@ -1,4 +1,4 @@
-"""Launch the search scrapper and the me_too scrapper"""
+"""Launch the scrappers """
 
 from hp_class_action.hinge_issue.scrap_data.scrap_search_query import webscrap_query_search
 from hp_class_action.hinge_issue.scrap_data.scrap_metoo import update_summary_metoo
@@ -8,6 +8,8 @@ from hp_class_action.hp_database.backup_mdb import backup_mdb_to_csv
 def launch_scrapper(max_pages: int = 10,
                     force_metoo_update: bool = False):
     print(f'_' * 100)
+
+    # Handle broken hinges
     print(f'Scrapping Broken Hinge Issues')
     backup_mdb_to_csv()
     webscrap_query_search(max_pages=max_pages)
