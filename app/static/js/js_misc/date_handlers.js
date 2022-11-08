8a025db362884a15f8ece78f4cb65ca0f45db7df
thinkcentre <mysqlprojet@gmail.com>
2022-11-08 20:07:57 +0000

    added js functions to convert timestamp str into required dates

---
+++ app/static/js/js_misc/date_handlers.js
@@ -0,0 +1,33 @@
+
+
+function timestamp_to_date(timestamp_str){
+    // Return the date of the timestamp
+    // timestamp shoud be of the form %Y-%m-%d %etc
+    date_str = String(timestamp_str).split(" ")[0];
+    return new Date(date_str);
+}
+
+function timestamp_to_day(timestamp_str){
+    // Return the day of the timestamp
+    // timestamp shoud be of the form %Y-%m-%d %etc
+    date_str = String(timestamp_str).split(" ")[0];
+    day_int = parseInt(date_str.split("-")[2]);
+    return day_int;
+    }
+function timestamp_to_month(timestamp_str){
+    // Return month of the timestamp
+    // timestamp shoud be of the form %Y-%m-%d %etc
+    date_str = String(timestamp_str).split(" ")[0];
+    month_int = parseInt(date_str.split("-")[1]);
+    return month_int;
+    }
+function timestamp_to_year(timestamp_str){
+    // Return year of the timestamp
+    // timestamp shoud be of the form %Y-%m-%d %etc
+    date_str = String(timestamp_str).split(" ")[0];
+    year_int = parseInt(date_str.split("-")[0]);
+    return year_int;
+    }
+
+
+console.log(timestamp_to_day("2022-01-30"));
