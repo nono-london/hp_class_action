6d10e04b07fbf600671a5447d97917976ab65a59
thinkcentre <mysqlprojet@gmail.com>
2022-11-10 15:20:47 +0000

    dropbox handler function

--- app/static/js/element_handlers/dropbox_handlers.js
+++ app/static/js/element_handlers/dropbox_handlers.js
@@ -1,7 +1,8 @@
 
 // needs to add class "dropdown-menu" in the button container
-function dropbox_active_inactive(button_clicked_element){
-    const dropdowns =  button_clicked_element.closest(".dropdown-menu").querySelectorAll(".dropdown-menu li a");
+function dropbox_active_inactive(button_clicked_element, text_prefix){
+    const dropdown_nodes =  button_clicked_element.closest(".dropdown-menu").querySelectorAll(".dropdown-menu li a");
+    const dropdown_container = button_clicked_element.closest(".button-active-handler").querySelector(".dropdown-toggle");
     //console.log(dropdowns);
     // Reset all buttons to inactive
     [... dropdowns].forEach( function (element){
@@ -9,8 +10,10 @@ function dropbox_active_inactive(button_clicked_element){
       //console.log(element);
       element.className="dropdown-item";
     });
-    //mark clicked button with active class
+    // mark clicked button with active class
     button_clicked_element.className="dropdown-item active";
+    // update main text container
+    dropdown_container.innerHTML =`${text_prefix}: ${button_clicked_element.innerHTML }`;
 
 
   }
