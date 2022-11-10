e305e0c8040a2fbbd6da803487b46a5fef19d27d
hplaptop <mysqlproject@gmail.com>
2022-11-10 11:38:44 +0100

    added chart builder function

--- app/static/js/chartjs_handlers/chart_builders.js
+++ app/static/js/chartjs_handlers/chart_builders.js
@@ -1,7 +1,12 @@
 // https://css-tricks.com/the-many-ways-of-getting-data-into-charts/
+// uses chartjs
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.js" integrity="sha512-d6nObkPJgV791iTGuBoVC9Aa2iecqzJRE0Jiqvk85BhLHAPhWqkuBiQb1xz2jvuHNqHLYoN3ymPfpiB1o+Zgpw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
 
 const chartBackgroundColor = [ // Specify custom colors
   'rgba(255, 99, 132, 0.2)',
+  'rgba(255, 130, 132, 0.2)',
   'rgba(54, 162, 235, 0.2)',
   'rgba(255, 206, 86, 0.2)',
   'rgba(75, 192, 192, 0.2)',
@@ -11,6 +16,7 @@ const chartBackgroundColor = [ // Specify custom colors
 
 const chartBorderColor = [ // Add custom color borders
   'rgba(255,99,132,1)',
+  'rgba(255, 130, 132, 1)',
   'rgba(54, 162, 235, 1)',
   'rgba(255, 206, 86, 1)',
   'rgba(75, 192, 192, 1)',
@@ -19,48 +25,9 @@ const chartBorderColor = [ // Add custom color borders
 ];
 
 
-function jsChartBuilder(labels, values, chartTitle, chartCanvasId, chartType) {
-  // var ctx = document.getElementById("myChart").getContext('2d');
-  var myChart = new Chart(chartCanvasId, {
-    type: chartType,
-    data: {
-      labels: labels, // Our labels
-      datasets: [{
-        label: chartTitle, // Name the series
-        data: values, // Our values
-        backgroundColor: [ // Specify custom colors
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [ // Add custom color borders
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1 // Specify bar border width
-      }]
-    },
-    options: {
-      responsive: true, // Instruct chart js to respond nicely.
-      maintainAspectRatio: false, // Add to prevent default behavior of full-width/height 
-    }
-  });
-  return myChart;
-};
-
 
 // this function works for sure
-// uses chartjs
-//     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.js" integrity="sha512-d6nObkPJgV791iTGuBoVC9Aa2iecqzJRE0Jiqvk85BhLHAPhWqkuBiQb1xz2jvuHNqHLYoN3ymPfpiB1o+Zgpw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-function createChart(canvas_id, chart_type, color_slice_start, color_slice_end,
+function chartJsBuilder(canvas_id, chart_type, color_slice_start, color_slice_end,
   x_values, y_values, chart_title) {
   const chart_graph = new Chart(canvas_id, {
     type: chart_type,
@@ -73,30 +40,17 @@ function createChart(canvas_id, chart_type, color_slice_start, color_slice_end,
         //maxBarThickness: 8,
         //minBarLength: 200,
         data: y_values,
-        backgroundColor: [ // Specify custom colors
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 130, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ].slice(color_slice_start, color_slice_end),
-        borderColor: [ // Add custom color borders
-          'rgba(255,99,132,1)',
-          'rgba(255, 130, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ].slice(color_slice_start, color_slice_end),
+        backgroundColor: chartBackgroundColor.slice(color_slice_start, color_slice_end),
+        borderColor: chartBorderColor.slice(color_slice_start, color_slice_end),
         borderWidth: 1 // Specify bar border width
 
 
       }]
     },
-
+    options: {
+      responsive: true, // Instruct chart js to respond nicely.
+      maintainAspectRatio: true, // Add to prevent default behavior of full-width/height 
+    }
   }
   )
 
