56b2391870999bee65e57fa63344821fc793e9eb
thinkcentre <mysqlprojet@gmail.com>
2022-11-11 21:27:53 +0000

    cleaning

--- app/static/js/chartjs_handlers/chart_builders.js
+++ app/static/js/chartjs_handlers/chart_builders.js
@@ -30,40 +30,6 @@ const chartBorderColor = [ // Add custom color borders
 
 
 
-// this function works for sure
-function chartJsBuilder(canvas_id, chart_type, color_slice_start, color_slice_end,
-  x_values, y_values, chart_title) {
-  const chart_graph = new Chart(canvas_id, {
-    type: chart_type,
-    data: {
-      labels: x_values,
-      datasets: [{
-        label: chart_title,
-        //barPercentage: 0.5,
-        //barThickness: 6,
-        //maxBarThickness: 8,
-        //minBarLength: 200,
-        data: y_values,
-        backgroundColor: chartBackgroundColor.slice(color_slice_start, color_slice_end),
-        borderColor: chartBorderColor.slice(color_slice_start, color_slice_end),
-        borderWidth: 1 // Specify bar border width
-
-
-      }]
-    },
-    options: {
-      responsive: true, // Instruct chart js to respond nicely.
-      maintainAspectRatio: true, // Add to prevent default behavior of full-width/height 
-    }
-  }
-  )
-  // return chart so that it can be destroyed before calling this function if it already exists
-  // define the chart in a var golbal variable on document
-  return chart_graph;
-
-}
-
-
 function chartJsBuilderMultipleDatasets(canvas_id, chart_type, datasets, serie_names, stacked) {
     // datasets should be composed of a list of list containing x_values, and y_values
     // [ [x_values, y_values],
