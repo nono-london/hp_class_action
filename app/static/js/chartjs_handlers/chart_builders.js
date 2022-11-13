a590db8beb1fea5b3724f0782cf8bd8d3d8198b0
hplaptop <mysqlproject@gmail.com>
2022-11-13 01:50:45 +0100

    problem wwith sorting

--- app/static/js/chartjs_handlers/chart_builders.js
+++ app/static/js/chartjs_handlers/chart_builders.js
@@ -6,7 +6,7 @@
 
 const chartBackgroundColor = [ // Specify custom colors
   'rgba(255, 99, 132, 0.2)',
-  
+
   'rgba(54, 162, 235, 0.2)',
   'rgba(255, 206, 86, 0.2)',
   'rgba(75, 192, 192, 0.2)',
@@ -18,7 +18,7 @@ const chartBackgroundColor = [ // Specify custom colors
 
 const chartBorderColor = [ // Add custom color borders
   'rgba(255,99,132,1)',
-  
+
   'rgba(54, 162, 235, 1)',
   'rgba(255, 206, 86, 1)',
   'rgba(75, 192, 192, 1)',
@@ -31,51 +31,51 @@ const chartBorderColor = [ // Add custom color borders
 
 
 function chartJsBuilderMultipleDatasets(canvas_id, chart_type, datasets, serie_names, stacked) {
-    // datasets should be composed of a list of list containing x_values, and y_values
-    // [ [x_values, y_values],
-    //   [x_values, y_values],
-    //  ]
-    // getting unique x_values
-    let x_values = getXValues(datasets);
-    function getXValues(chart_datasets){
-      let x_values_set = new Set();
-      chart_datasets.forEach(function(dataset){
-        //select x_values 1st list
-        dataset.forEach(function(row_dict){
-          // console.log("start row dict");
-          // console.log(row_dict);
-          x_values_set.add(Object.values(row_dict)[0]);
-          // console.log(x_values_set);
-        
-        })
+  // datasets should be composed of a list of list containing x_values, and y_values
+  // [ [x_values, y_values],
+  //   [x_values, y_values],
+  //  ]
+  // getting unique x_values
+  let x_values = getXValues(datasets);
+  function getXValues(chart_datasets) {
+    let x_values_set = new Set();
+    chart_datasets.forEach(function (dataset) {
+      //select x_values 1st list
+      dataset.forEach(function (row_dict) {
+        // console.log("start row dict");
+        // console.log(row_dict);
+        x_values_set.add(Object.values(row_dict)[0]);
+        // console.log(x_values_set);
+
       })
+    })
 
-      return Array.from(x_values_set);
-    }
-    
-    console.log("start du x");
-    console.log(x_values);
-    console.log("end du x");
+    return Array.from(x_values_set);
+  }
 
-    // create datasets for chartjs
-  let chart_datasets=[];
-  for (let i=0;i<serie_names.length;i++){
+  // console.log("start du x");
+  // console.log(x_values);
+  // console.log("end du x");
+
+  // create datasets for chartjs
+  let chart_datasets = [];
+  for (let i = 0; i < serie_names.length; i++) {
     let y_values = [];
     let temp_dataset = addMissingDataToYaxis(x_values, datasets[i]);
     // fills up y_values
-    temp_dataset.forEach(function(row_dict){
+    temp_dataset.forEach(function (row_dict) {
       //select y_values: 2nd list
       y_values.push(Object.values(row_dict)[1]);
     })
-    const chart_dataset = create_chartjs_dataset(y_values,serie_names[i], i, i+1);
+    const chart_dataset = create_chartjs_dataset(y_values, serie_names[i], i, i + 1);
     chart_datasets.push(chart_dataset);
   }
 
-  
+
 
   let scale_option = {}
   // scale/stack bars
-  if (stacked===true){
+  if (stacked === true) {
     console.log("stacked is True with ===")
     scale_option = {
       x: {
@@ -108,41 +108,41 @@ function chartJsBuilderMultipleDatasets(canvas_id, chart_type, datasets, serie_n
 }
 
 
-function addMissingDataToYaxis(x_values, dataset){
+function addMissingDataToYaxis(x_values, dataset) {
   // take 1st row of dataset inorder to have their dict names
   // will add data using this Object.values(row_1)[0]);
   // console.log("x_values");
   // console.log(x_values);
   // console.log("x_values");
-  let dict_template  = dataset[0];
+  let dict_template = dataset[0];
   // console.log("dict_template");
   const x_key = Object.keys(dict_template)[0];
   const y_key = Object.keys(dict_template)[1];
   // console.log("x_key");
   // console.log(x_key);
-  
+
   // console.log("dataset");
   // console.log(dataset);
   missing_data = [];
-  
-  for (let i=0;i<x_values.length;i++ ){
+
+  for (let i = 0; i < x_values.length; i++) {
     // console.log('xvalues            '+i);
     // console.log(dataset);
-    j=0;
-    let x_value_found=false;
-    while (j<dataset.length){
-      if (x_values[i]==dataset[j][x_key] ){
+    j = 0;
+    let x_value_found = false;
+    while (j < dataset.length) {
+      if (x_values[i] == dataset[j][x_key]) {
         //console.log("Equal values:");
         // console.log(x_values[i]);
         // console.log(dataset[j][x_key]);
-        x_value_found=true;
+        x_value_found = true;
         break;
-       }
-      j++;
       }
-    if (!x_value_found){
+      j++;
+    }
+    if (!x_value_found) {
       missing_data.push(x_values[i]);
-      dataset.splice(i, 0, {[x_key]:x_values[i], [y_key]:0});
+      dataset.splice(i, 0, { [x_key]: x_values[i], [y_key]: 0 });
 
     }
   }
@@ -151,14 +151,15 @@ function addMissingDataToYaxis(x_values, dataset){
   //   console.log(missing_data);
   //   console.log("New dataset");
   //   console.log(dataset);
-    
+
   // }
 
 
   return dataset;
 }
 
-function create_chartjs_dataset(y_values, serie_name, color_slice_start, color_slice_end){
+// Simpler version which doesn't handle multiple series on same chart
+function create_chartjs_dataset(y_values, serie_name, color_slice_start, color_slice_end) {
   let chart_dataset = {
     label: serie_name,
     //barPercentage: 0.5,
