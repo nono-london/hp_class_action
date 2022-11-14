886c989905e3260ae609bad14a4362fb7cb8030b
thinkcentre <mysqlprojet@gmail.com>
2022-11-14 18:05:09 +0000

    adding rest_api

--- app/main.py
+++ app/main.py
@@ -120,12 +120,17 @@ def contact_form():
                  }
     return render_template('contact_form.html', page_vars=page_vars)
 
+
 @app.route("/rest_api", methods=['GET'])
 def rest_api():
     get_customer_ip_address()
     page_title = "REST API"
+    h2_text = """How to use the REST API"""
+    page_vars = {'title': page_title,
+                 'h2_text': h2_text,
 
-    return render_template('rest_api.html', page_vars={'title': page_title, })
+                 }
+    return render_template('rest_api.html', page_vars=page_vars)
 
 
 if __name__ == '__main__':
