69630c1e555a7215c9753f8e9226f9fce7485146
hplaptop <mysqlproject@gmail.com>
2022-11-15 22:36:15 +0100

    HPLAPTOP: cleaned test

--- tests/test_external_ip_address.py
+++ tests/test_external_ip_address.py
@@ -2,7 +2,7 @@ from hp_class_action.app_config import get_external_ip_address
 
 
 def test_ip_address():
-    get_external_ip_address()
+    assert isinstance(get_external_ip_address(), str) and len(get_external_ip_address()) > 0
 
 
 if __name__ == '__main__':
