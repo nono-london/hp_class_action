61a799d65cd405a81c6a82b4c365e9adba061ca6
thinkcentre <mysqlprojet@gmail.com>
2022-11-15 03:44:00 +0000

    grouping by day

--- app/static/js/grouping_handlers/group_by_dates.js
+++ app/static/js/grouping_handlers/group_by_dates.js
@@ -1,32 +1,70 @@
 // https://stackoverflow.com/questions/69615993/how-to-group-array-of-dates-and-sum-value-by-day-1hour-6hours-8hours-week-m
 
 
-function get_year_month_parts(date_as_date){
+function get_year_month_day_parts(date_as_date){
     month = timestamp_to_month(date_as_date);
     year = timestamp_to_year(date_as_date);
-    
-    return {year, month};
+    day =  timestamp_to_day(date_as_date);
+    return {year, month, day};
 
 }
 
-function group_by_month(arr, date_key) {
-    return Object.values(
-      arr.reduce((a, { date_key: date_as_date, date_key: value }) => {
-        const { year, month } = get_year_month_parts(date_as_date) ;
-        const key = `${year}/${month}`;
-        // using logical nullish assignment
-        //(a[key] ??= { color: 'Blue?', value: 0, label: key }).value += value;
-  
-        // or written out long hand
-        if (a[key] === undefined) {
-          a[key] = { color: 'Blue?', value: 0, date_key: key };
-        }
-  
-        a[key].value += 1;
-  
-        return a;
-      }, {}),
-    );
+function group_by_year(arr) {
+  return Object.values(
+    arr.reduce((a, { post_datetime: date_as_date, claims: count }) => {
+      // console.log("a");
+      // console.log(a);
+      // console.log("date_as_date");
+      // console.log(date_as_date);
+      // console.log(a);
+      const year = timestamp_to_year(date_as_date) ;
+      const key = `${year}`;
+
+      if (a[key] === undefined) {
+        a[key] = {post_datetime: key, count: 0,  };
+      }
+
+      a[key].count += 1;
+
+      return a;
+    }, {}),
+  );
 }
-const group_by_m_dataset = group_by_month(json_dataset['data'], 'post_id');
-console.log(group_by_m_dataset);
+
+function group_by_month(arr) {
+  return Object.values(
+    arr.reduce((a, { post_datetime: date_as_date, claims: count }) => {
+      const { year, month } = get_year_month_day_parts(date_as_date) ;
+      const key = `${year}-${('0'+month).slice(-2)}`;
+
+      if (a[key] === undefined) {
+        a[key] = {post_datetime: key, count: 0,  };
+      }
+
+      a[key].count += 1;
+
+      return a;
+    }, {}),
+  );
+}
+
+function group_by_day(arr, date_key) {
+
+  return Object.values(
+    arr.reduce((new_array, { [date_key]: date_as_date, date_key: count }) => {
+
+      const { year, month, day } = get_year_month_day_parts(date_as_date) ;
+      const key = `${year}-${('0'+month).slice(-2)}-${('0'+day).slice(-2)}`;
+
+      if (new_array[key] === undefined) {
+        new_array[key] = {[date_key]: key, count: 0,  };
+      }
+
+      new_array[key].count += 1;
+
+      return new_array;
+    }, {}),
+  );
+}
+
+
