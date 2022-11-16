91a3ae22ce65d4609d2f6bbe7ae10d9993e75e39
thinkcentre <mysqlprojet@gmail.com>
2022-11-16 21:10:16 +0000

    THINKCENTRE: added website_visitor_info table

--- hp_class_action/hp_website/visitors_ip/ip_location_to_mdb.py
+++ hp_class_action/hp_website/visitors_ip/ip_location_to_mdb.py
@@ -1,11 +1,29 @@
 import csv
 from json import loads
-from typing import Dict, Union
+from typing import Dict, Union, List, Tuple
 from urllib.error import URLError
 from urllib.request import Request, urlopen
 
 from hp_class_action.app_config import (get_hp_website_visitors_file_path)
+from hp_class_action.hp_database.mdb_handlers import execute_query
 
+
+def read_visitors_file(file_has_header: bool = True) -> tuple:
+    """Return a tuple of list: (header, rows), where headers can be null"""
+    # https://www.programiz.com/python-programming/file-operation
+    # https://stackoverflow.com/questions/24662571/python-import-csv-to-list
+    file_path = get_hp_website_visitors_file_path()[0]
+
+    with open(file_path, 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        # print(list(csv_reader))
+        csv_table = list(csv_reader)
+        if file_has_header:
+            csv_headers = csv_table[0]
+            csv_rows = csv_table[1:]
+        else:
+            csv_rows = csv_table
+    return csv_headers, csv_rows
 
 def get_ip_info(ip_address: str) -> Union[None, Dict]:
     # https://ipapi.co/#pricing
@@ -32,25 +50,40 @@ def get_ip_info(ip_address: str) -> Union[None, Dict]:
     json_data = loads(data.decode(encoding))
     return json_data
 
+def upload_to_database(csv_row:List, row_headers:List, ip_info:Dict)->bool:
+    sql_query = """
+                INSERT INTO `hp_trial`.`website_visitors_info`
+                    (`visit_datetime`, `visit_url`, `ip_address`, `city`, `region`,
+                    `country_code`, `country_code_iso3`, `country_name`, `coordinate`, `timezone`,
+                    `utc_offset`, `org`
+                    )
+                VALUES( %s,  %s, %s, %s, %s,
+                         %s, %s, %s, 
+                            POINT(%s,%s), %s,
+                          %s, %s
+                    );
+                """
 
-def read_visitors_file(file_has_header: bool = True) -> tuple:
-    """Return a tuple of list: (header, rows), where headers can be null"""
-    # https://www.programiz.com/python-programming/file-operation
-    # https://stackoverflow.com/questions/24662571/python-import-csv-to-list
-    file_path = get_hp_website_visitors_file_path()[0]
+    parameters:Tuple = (
+        csv_row[row_headers.index("visit_datetime")], csv_row[row_headers.index("visited_url")],
+                ip_info['ip'], ip_info['city'], ip_info['region'],
+        ip_info['country_code'], ip_info['country_code_iso3'], ip_info['country_name'],
+                ip_info['latitude'], ip_info['longitude'], ip_info['country_code'],
+        ip_info['utc_offset'], ip_info['org']
+    )
+    execute_query(sql_query=sql_query, variables=parameters)
 
-    with open(file_path, 'r', encoding='utf-8') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        # print(list(csv_reader))
-        csv_table = list(csv_reader)
-        if file_has_header:
-            csv_headers = csv_table[0]
-            csv_rows = csv_table[1:]
-        else:
-            csv_rows = csv_table
-    return csv_headers, csv_rows
+def upload_data():
+    headers, rows = read_visitors_file(file_has_header=True)
+    for row in rows:
+        ip_address = row[headers.index("http_x_real_ip")]
+        ip_info = get_ip_info(ip_address)
+        upload_to_database(csv_row=row,row_headers=headers, ip_info=ip_info)
+        return
 
 
 if __name__ == '__main__':
+    upload_data()
+    exit(0)
     print(read_visitors_file())
     # get_ip_info("104.47.70.126")
