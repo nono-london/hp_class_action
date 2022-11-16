0bcdeb210aeaec982b7f05f6c5e8e5df6ffe8d82
thinkcentre <mysqlprojet@gmail.com>
2022-11-16 21:57:55 +0000

    corrected cdn import ros per page button now works

--- app/static/js/table_handlers/bootstrap_table.js
+++ app/static/js/table_handlers/bootstrap_table.js
@@ -4,10 +4,10 @@ function table_populator(table_dataset, table_to_populate, dataSetHasBeenJONed=f
     //table_to_populate.bootstrapTable('destroy');
     // console.log('inner table populator');
     // console.log(table_dataset);
-    table_dataset.forEach(element => {
-        console.log(element);
+    // table_dataset.forEach(element => {
+    //     console.log(element);
         
-    });
+    // });
 
     // table_to_populate.bootstrapTable({
     //     data: table_dataset
@@ -19,17 +19,17 @@ function table_populator(table_dataset, table_to_populate, dataSetHasBeenJONed=f
 
         if (table_dataset !=null) {
             if (dataSetHasBeenJONed==true){
-                console.log("boostrap-table function DO NOt need to be parsed");
+                // console.log("boostrap-table function DO NOt need to be parsed");
                 table_to_populate.bootstrapTable({data:table_dataset});    
             }
             else{
-                console.log("boostrap-table data need to be parsed");
+                // console.log("boostrap-table data need to be parsed");
                 table_dataset = JSON.parse(table_dataset);
                 table_to_populate.bootstrapTable({data: table_dataset});    
             }
             
         } else {
-            console.log('No data found for table:'+table_to_populate);
+            // console.log('No data found for table:'+table_to_populate);
             //table_to_populate.bootstrapTable({data:null});
             table_to_populate.bootstrapTable();
         }
