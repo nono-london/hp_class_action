1c5e499b9b68d8e6d8985771e00bdbe58ade2250
hplaptop <mysqlproject@gmail.com>
2022-11-17 02:12:48 +0100

    HPLAPTOP: ARIN API handler

--- hp_class_action/hp_website/visitors_ip/arin_whois.py
+++ hp_class_action/hp_website/visitors_ip/arin_whois.py
@@ -1,5 +1,5 @@
 from json import loads
-from typing import Dict, Union, List
+from typing import Dict, List
 from urllib.error import URLError
 from urllib.request import Request, urlopen
 
@@ -9,9 +9,10 @@ from hp_class_action.hp_database.mdb_handlers import (fetch_query)
 def get_visitors_info() -> List[Dict]:
     """Returns True if csv row has already been uploaded to mdb"""
     sql_query = """
-        SELECT *
-        FROM website_visitors_info
-        ORDER BY visit_datetime    
+        SELECT ip_address, country_name, city, org, COUNt(*) "visits"
+        FROM hp_trial.website_visitors_info
+        GROUP BY ip_address, country_name, city, org
+        ORDER BY visit_datetime ASC   
     """
     parameters = None
     results = fetch_query(sql_query=sql_query, variables=parameters)
@@ -20,10 +21,9 @@ def get_visitors_info() -> List[Dict]:
 
 
 def get_json_request(url):
-
     req = Request(url)
     try:
-        response = urlopen(req)
+        response = urlopen(url=req, timeout=20, )
     except URLError as e:
         if hasattr(e, 'reason'):
             print('We failed to reach a server.')
@@ -41,14 +41,18 @@ def get_json_request(url):
     return response
 
 
-def get_arin_info(ip_address: str) -> Union[None, Dict]:
+def print_arin_info(mdb_row: dict) -> bool:
     """https://www.arin.net/resources/registry/whois/rdap/"""
     """ same without using requests lib
         https://github.com/rush-dev/arin-whois/blob/master/whois.py
     """
     response_format = "json"
-    url = f'http://whois.arin.net/rest/ip/{ip_address}.{response_format}'
-    print(url)
+    url = f'http://whois.arin.net/rest/ip/{mdb_row["ip_address"]}.{response_format}'
+
+    print("#" * 50, "ARIN Information", "#" * 50)
+    print(f"getting info for ip: {mdb_row['ip_address']} "
+          f"in country: {mdb_row['country_name']} "
+          f"for org: {mdb_row['org']}")
     ip_response = get_json_request(url=url)
 
     print(ip_response.get('net').get('resources').get("limitExceeded"))
@@ -57,8 +61,12 @@ def get_arin_info(ip_address: str) -> Union[None, Dict]:
     end_address = ip_response['net']['endAddress']['$']
     handle = ip_response['net']['handle']['$']
     name = ip_response['net']['name']['$']
-    org_name = ip_response['net']['orgRef']['@name']
-    org_handle = ip_response['net']['orgRef']['@handle']
+    try:
+        org_name = ip_response['net']['orgRef']['@name']
+        org_handle = ip_response['net']['orgRef']['@handle']
+    except Exception as ex:
+        print(f'Error with org_name/org_handle: {ex}')
+        return False
     last_updated = ip_response['net']['updateDate']['$']
     rest_link = ip_response['net']['ref']['$']
 
@@ -79,56 +87,47 @@ def get_arin_info(ip_address: str) -> Union[None, Dict]:
 
     try:
         cidr = ip_response['net']['netBlocks']['netBlock']['cidrLength']['$']
-
     except TypeError:
         cidr = ip_response['net']['netBlocks']['netBlock'][0]['cidrLength']['$']
 
     try:
         net_type = ip_response['net']['netBlocks']['netBlock']['description']['$']
-
     except TypeError:
         net_type = ip_response['net']['netBlocks']['netBlock'][0]['description']['$']
 
     try:
         parent_name = ip_response['net']['parentNetRef']['@name']
         parent_handle = ip_response['net']['parentNetRef']['@handle']
-
     except KeyError:
         parent_name = ''
         parent_handle = ''
 
     try:
         origin_as = ip_response['net']['originASes']['originAS'][0]['$']
-
     except KeyError:
         origin_as = ''
 
     try:
         reg_date = ip_response['net']['registrationDate']['$']
-
     except KeyError:
         reg_date = ''
 
     try:
         org_reg_date = org_response['org']['registrationDate']['$']
-
     except KeyError:
         org_reg_date = ''
 
     try:
         state = org_response['org']['iso3166-2']['$']
-
     except KeyError:
         state = ''
 
     try:
         street = org_response['org']['streetAddress']['line']['$']
-
     except TypeError:
         street = org_response['org']['streetAddress']['line'][0]['$']
 
     # Output to terminal
-    print(f'You searched for: {ip_address}\n')
     print('Network')
     print(f'NetRange:         {start_address} - {end_address}')
     print(f'CIDR:             {start_address}/{cidr}')
@@ -152,8 +151,21 @@ def get_arin_info(ip_address: str) -> Union[None, Dict]:
     print(f'RegistrationDate: {org_reg_date}')
     print(f'LastUpdated:      {org_last_updated}')
     print(f'RESTful Link:     {org_rest_link}')
+    return True
+
+
+def print_all_visitors_info():
+    visitors = get_visitors_info()
+    errors = []
+    for visitor in visitors:
+        if not print_arin_info(mdb_row=visitor):
+            errors.append(visitor)
+    if errors:
+        print("-" * 100)
+        print(f"ARIN couldn't find information for the followings IPs:\n"
+              f"{errors}")
 
 
 if __name__ == '__main__':
     get_visitors_info()
-    print(get_arin_info(ip_address="172.58.187.155"))
+    print_all_visitors_info()
